94737d0c1734e0324d4af189d3bd76fa73ab8023
Santripta Sharma <santripta@gmail.com>
2020-11-06 18:34:11 +0530

    nav

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let nav = document.getElementsByTagName("nav")[0];
+    
+    nav.addEventListener("mouseenter", () => {
+        nav.classList.add("open");
+
+        nav.addEventListener("mouseleave", () => {
+            nav.classList.remove("open");
+        });
+    });
+});
