7dd323107272855b15576267e1e8cdaa24fa8f1c
Santripta Sharma <santripta@gmail.com>
2020-12-20 11:56:29 +0530

    thing

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         nav.addEventListener("mouseleave", () => {
             nav.classList.remove("open");
-            nav.removeEventListener("mouseleave");
+            nav.removeEventListener("mouseleave", this);
         });
     });
 });
--- src/js/team.js
+++ src/js/team.js
@@ -5,14 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     {
         (() => 
         {
-            active = false;
             let parent = bio.parentElement;
-            let socials = Array.prototype.slice.call(parent.getElementsByTagName("a"));
-            console.log(socials);
             parent.addEventListener("click", (e) => {
-                if(socials.includes(e.target)) return;
-                active = !active;
-                parent.classList.toggle("active", active);
+                console.log("ok");
+                parent.classList.toggle("active");
             });
         })();
     }
